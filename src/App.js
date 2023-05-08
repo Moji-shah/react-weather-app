82e368e675d43863029d3eac469a45004830e105
Mozhgan <mojganshah@gmail.com>
2023-05-08 21:17:14 +0430

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Mozhgan Shahmohammadi and is{" "} 
-        <a
-          href="https://github.com/Moji-shah/react-weather-app"
-          target="_blank"
-        > open-sourced </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <button className="btn btn-primary">search</button>
+        <footer>
+          This project was coded by Mozhgan Shahmohammadi and is{" "}
+          <a
+            href="https://github.com/Moji-shah/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
