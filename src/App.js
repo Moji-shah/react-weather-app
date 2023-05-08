8477e059d7f19c70497218282af0913b379a6afd
Mozhgan <mojganshah@gmail.com>
2023-05-08 20:57:31 +0430

    Added Git repo link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Mozhgan Shahmohammadi and is{" "} 
+        <a
+          href="https://github.com/Moji-shah/react-weather-app"
+          target="_blank"
+        > open-sourced </a>
+      </footer>
+    </div>
+  );
 }
