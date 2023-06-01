72142bef080f2ea71e4dac5ee1e4b4be4d821f86
Mozhgan <mojganshah@gmail.com>
2023-06-01 18:52:16 +0430

    final bg image

--- src/App.js
+++ src/App.js
@@ -4,31 +4,29 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="Body">
-      <div className="App">
-        <div className="container">
-          <Weather defaultCity="Tehran" />
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Tehran" />
 
-          <footer className="footer">
-            Coded by Mozhgan Shahmohammadi and is
-            <a
-              href="https://github.com/Moji-shah/react-weather-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              open-sourced
-            </a>{" "}
-            and hosted on{" "}
-            <a
-              href="https://chipper-meringue-10e1d4.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Netlify
-            </a>
-          </footer>
-        </div>
+        <footer className="footer">
+          Coded by Mozhgan Shahmohammadi and is
+          <a
+            href="https://github.com/Moji-shah/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://chipper-meringue-10e1d4.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
